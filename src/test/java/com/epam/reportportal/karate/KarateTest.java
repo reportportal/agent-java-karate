941db52d42504f8bb65f84bf24f14cfb119cb4ab
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-12-12 15:56:51 +0300

    Do not output Cucumber to avoid failures in CI

--- src/test/java/com/epam/reportportal/karate/KarateTest.java
+++ src/test/java/com/epam/reportportal/karate/KarateTest.java
@@ -11,7 +11,7 @@ class KarateTest {
 	void testParallel() {
 		Results results = KarateReportPortalRunner
 				.path("classpath:feature")
-				.outputCucumberJson(true)
+				.outputCucumberJson(false)
 				.tags("~@ignore", "@To_run")
 				.parallel(2);
 		assertEquals(0, results.getFailCount());
